11cbee54d0d243b1d00b62b72caa18a6b59ac722
cha80537 <cha80537@gmail.com>
2025-03-17 18:41:46 +0900

    v0.2 O(1)

--- week02.py
+++ week02.py
@@ -1,10 +1,7 @@
 # alt + shift + F10
 n = int(input("정수 입력 :"))
-result = 0
-i = 1
-while i <= n:
-    result = result + i
-    i = i + 1
+result = n * (n + 1) // 2 # O(1)
+
 print(result)
 
 # n이 1개 -> 선형시간
