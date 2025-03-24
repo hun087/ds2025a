e27671df946314d95ea5a3a9fea38cd47bb8b97c
cha80537 <cha80537@gmail.com>
2025-03-24 18:36:21 +0900

    v7.2 array

--- week03.py
+++ week03.py
@@ -1,13 +1,19 @@
-def inters(l1, l2):
-    s1 = set(l1)
-    s2 = set(l2)
-    #return list(s1.intersection(s2))
-    #return list(s1.union(s2))
-    #return list(s1 | s2)
-    #return list(s1 - s2)
-    return list(s1 & s2)
+def my_zip(l1, l2):
+    l3 = list()
+    length = 0
+    if len(l1) < len(l2):
+        length = len(l1)
+    else:
+        length = len(l2)
+
+    for i in range(length):
+        l3.append((l1[i], l2[i]))
+    return l3
 
 
-l1 = [45, 5, 22, 31, 7, 19]
-l2 = [22, 1, 5, 2, 7, 28, 27, 19, 13, 41]
-print(inters(l1, l2))
+groups = ['HOT', 'Seventeen', 'Black Pink', 'NJZ']
+ratings = [1, 2, 4, 3, 100]
+# ratings = [1, 2, 4, 3]
+
+#group_rating = my_zip(zip(groups, ratings))
+print(my_zip(groups, ratings))
