77de488a1da2a31c184a0acc1f1a40aef01ee8a1
cha80537 <cha80537@gmail.com>
2025-03-31 17:44:34 +0900

    v1.5 linked list

--- week04.py
+++ week04.py
@@ -18,20 +18,32 @@ class LinkedList:
             current = current.link
         current.link = Node(data)
 
+
+    #def is_find(self, target):    # is - 명제
+    def search(self, target):
+        current = self.head
+        while current.link:
+            if target == current.data:
+                return f"{target}을(를) 찾았습니다!"
+            else:
+                current = current.link
+        return f"{target}은(는) 링크드 리스트 안에 존재하지 않습니다."
+
+
+
     def __str__(self):
         current = self.head
         result = ""
         while current is not None:
-            # print(current.data)
             # result = result + str(current.data) + " -> "
             result = result + f"{current.data} -> "
             current = current.link
         return result + "END"
-        #return "Linked list!"
 
 ll = LinkedList()
 ll.append(8)
 ll.append(10)
 ll.append(-9)
 print(ll)
-
+print(ll.search(99))
+print(ll.search(10))
