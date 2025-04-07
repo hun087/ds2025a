e688a093e4e754f49ba92fd489084133b8cae500
cha80537 <cha80537@gmail.com>
2025-04-07 10:02:28 +0900

    v1.5 linked list

--- week04.py
+++ week04.py
@@ -1,67 +1,71 @@
+import random
+
 
 class Node:
-    def __init__(self, data, links=None):
+    def __init__(self, data, link=None):
         self.data = data
-        self.link = links
-
-# a = None("ABC")
-class LinkedList:
-    def __init__(self):
-        self.head = None    #false
 
 
-    def append(self, data):
-        if not self.head:       # if~return 1번만
-            self.head = Node(data)  #data -> 8
-            return
-        current = self.head     # current - 지역변수
-        while current.link:
+@ @-9
+
+, 14 + 11, 25 @ @
+
+
+def __init__(self):
+    self.head = None
+
+
+def append(self, data):
+    if not self.head:  # 링크드리스트가 노드가 하나도 없는 상태면
+        self.head = Node(data)  # 새 노드를 head에 연결
+    if not self.head:
+        self.head = Node(data)
+        return
+    current = self.head
+    while current.link:
+        current = current.link  # 다음노드로 이동
+        current = current.link
+    current.link = Node(data)
+
+
+def search(self, target):
+    current = self.head
+    while current.link:
+        if current.data == target:
+            return f"{target}을(를) 찾았습니다"
+        else:
             current = current.link
-        current.link = Node(data)
-
-    def remove(self, target):
-        current = self.head
-        if self.head.data == target:
-            self.head = self.head.link
-            current.link = None
-            return
-
-        previous = None     # previous - 지역변수
-        while current:
-            if target == current.data:
-                previous.link = current.link
-                current.link = None
-            previous = current
-            current = current.link
+    return f"{target}은(는) 링크드리스트 안에 존재하지 않습니다"
 
 
-    def search(self, target):
-        current = self.head
-        while current:   #bug fix
-            if target == current.data:
-                return f"{target}을(를) 찾았습니다!"
-            else:
-                current = current.link
-        return f"{target}은(는) 링크드 리스트 안에 존재하지 않습니다."
+def __str__(self):
+    node = self.head
+    out_texts = ""
 
-    def __str__(self):
-        current = self.head
-        result = ""
-        while current is not None:
-            # result = result + str(current.data) + " -> "
-            result = result + f"{current.data} -> "
-            current = current.link
-        return result + "END"
+
+@ @-26
+
+, 8 + 39, 17 @ @
+
+
+def __str__(self):
+    return out_texts + "end"
+
+
+# ll = LinkedList()
+# ll.append(8)
+# ll.append(10)
+# ll.append(-9)
+# print(ll)
+# print(ll.search(100))
+# print(ll.search(10))
 
 ll = LinkedList()
 ll.append(8)
 ll.append(10)
 ll.append(-9)
+for _ in range(10):
+    ll.append(random.randint(1, 30))
+
 print(ll)
-#print(ll.is_find(99))
-#print(ll.is_find(10))
-print(ll.search(99))
-print(ll.search(-9))
-ll.remove(-9)
-ll.remove(77)
-print(ll)
+print(ll.search(10))
