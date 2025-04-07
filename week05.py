966e51b5d46ef1dd09975cd1ba28a1e0d57e491b
cha80537 <cha80537@gmail.com>
2025-04-07 18:17:59 +0900

    v2.4 stack

--- week05.py
+++ week05.py
@@ -1,11 +1,18 @@
-s1 = []
-s1.append("Data structure")     # push
-s1.append("Database")
-print(s1[-1])   # peek
-print(s1)
-print(s1.pop()) # pop
-print(s1)
-print(s1.pop()) # pop
-print(s1)
-print(s1.pop()) # IndexError: pop from empty list
-print(s1)
+# print(1+2))
+def is_valid_parentheses(expression : str) -> bool: # type hint   bool - return type, str - parameter data type
+    stack = list()
+    for letter in expression:
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if len(stack) == 0:
+                return  False   # )1+2(), (1+2))
+            else:
+                stack.pop()
+    return len(stack) == 0  # (1+2), ((3*2)/2), ((3*2/2)
+
+print(is_valid_parentheses(")1+2()"))   # if len(stack) == 0: return  False 에 걸림
+print(is_valid_parentheses("(1+2))"))   # ""안 마지막 ) 에서 걸림
+print(is_valid_parentheses("(1+2)"))    # push 1번 pop 1번 - stack == 0
+print(is_valid_parentheses("((3*2)/2)"))    # push 2번 pop 2번 - stack == 0
+print(is_valid_parentheses("((3*2/2)")) # len(stack) - False (여는 괄호 더 많을때)
