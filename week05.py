f67b5038d7181968e32997aa2e64c62d33ddb084
cha80537 <cha80537@gmail.com>
2025-04-07 18:35:55 +0900

    v2.5 stack

--- week05.py
+++ week05.py
@@ -1,18 +1,18 @@
-# print(1+2))
-def is_valid_parentheses(expression : str) -> bool: # type hint   bool - return type, str - parameter data type
+def is_valid_brackets(expression : str) -> bool: # type hint   bool - return type, str - parameter data type
     stack = list()
+    brackets = {']': '[', '}': '{', ')': '('}
     for letter in expression:
-        if letter == "(":
+        if letter in brackets.values():     # [, {, ( -> brackets.values()
             stack.append(letter)
-        if letter == ")":
-            if len(stack) == 0:
-                return  False   # )1+2(), (1+2))
-            else:
-                stack.pop()
-    return len(stack) == 0  # (1+2), ((3*2)/2), ((3*2/2)
+        if letter in brackets.keys():       # ], }, ) -> brackets.keys()
+            if not stack or stack.pop() != brackets[letter]:
+                return  False       # stack 남아 있으면 False
+    return not stack
 
-print(is_valid_parentheses(")1+2()"))   # if len(stack) == 0: return  False 에 걸림
-print(is_valid_parentheses("(1+2))"))   # ""안 마지막 ) 에서 걸림
-print(is_valid_parentheses("(1+2)"))    # push 1번 pop 1번 - stack == 0
-print(is_valid_parentheses("((3*2)/2)"))    # push 2번 pop 2번 - stack == 0
-print(is_valid_parentheses("((3*2/2)")) # len(stack) - False (여는 괄호 더 많을때)
+print(is_valid_brackets("[({1+2)]}"))
+print(is_valid_brackets("[({1+2})]"))
+print(is_valid_brackets(")1+2()"))
+print(is_valid_brackets("(1+2))"))
+print(is_valid_brackets("(1+2)"))
+print(is_valid_brackets("((3*2)/2)"))
+print(is_valid_brackets("((3*2/2)"))
