b679fa35d74b5492eb891d2329b1416eb76d11dc
cha80537 <cha80537@gmail.com>
2025-04-07 17:49:54 +0900

    v2.3 stack

--- week05.py
+++ week05.py
@@ -1,40 +1,11 @@
-class Node:
-    def __init__(self, data, links=None):
-        self.data = data
-        self.link = links
-
-class Stack:
-    def __init__(self):
-        self.top = None
-
-
-    def push(self, data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node
-
-    def pop(self):
-        if self.top is None:
-            return "Stack is empty!"
-        popped_node = self.top
-        self.top = self.top.link
-        popped_node.link = None
-        return popped_node.data
-
-    def peek(self):
-        return self.top.data        #ex) 시험 () 넣기
-
-s1 = Stack()
-# print(s1.pop())
-s1.push("Data structure")
-s1.push("Database")
-# print(s1.pop())
-s1.pop() # 삭제만
-print(s1.peek())
-#for i in range(2):      # 선형 시간
-#    print(s1.pop())
-
-# size, is_empty 도 추가
+s1 = []
+s1.append("Data structure")     # push
+s1.append("Database")
+print(s1[-1])   # peek
+print(s1)
+print(s1.pop()) # pop
+print(s1)
+print(s1.pop()) # pop
+print(s1)
+print(s1.pop()) # IndexError: pop from empty list
+print(s1)
