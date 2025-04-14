d72c6a4ab7d24e854647b34ef414e5d716432ff3
cha80537 <cha80537@gmail.com>
2025-04-14 18:01:58 +0900

    v3.1 queue

--- week06.py
+++ week06.py
@@ -1,44 +1,13 @@
-class Node:
-    def __init__(self, data, link=None):
-        self.data = data
-        self.link = link
-
-
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear = None
-        self.size = 0
-
-    def enqueue(self, data):
-        self.size = self.size + 1
-        node = Node(data)
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.link = node
-            self.rear = node    # move
-
-
-    def dequeue(self):    # dequeue - 빼기
-        if self.front is None:  # q의 front 값이 none
-            raise IndexError("Queue is empty!") # q 비어있다
-        self.size = self.size - 1   # 1개 삭제
-        temp = self.front
-        self.front = self.front.link    # move
-        # self.front = self.rear    # 원소가 3개 이상 이면 코드 불가능
-        temp.link = None    # 순서 중요
-        if self.front is None:
-            self.rear = None
-        return temp.data
+from queue import Queue     # 클래스 설계 필요x
 
 q = Queue()
-q.enqueue("Data structure")     # front
-q.enqueue("Database")
-q.enqueue("Javascript")
-print(q.size, q.front.data, q.rear.data)
-print(q.dequeue())
-print(q.size, q.front.data, q.rear.data)
-print(q.dequeue())
-print(q.size, q.front.data, q.rear.data)
+q.put("Data structure")
+q.put("Database")
+q.put("Javascript")
+print(q.qsize())
+print(q.get())  #dequeue
+print(q.qsize())
+print(q.get())
+print(q.qsize())
+print(q.get())
+print(q.qsize())
