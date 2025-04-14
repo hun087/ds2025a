49e487d7dbb2e65ab43f1b95ec57e39cd6bf651d
cha80537 <cha80537@gmail.com>
2025-04-14 17:19:53 +0900

    v2.7 queue

--- week06.py
+++ week06.py
@@ -18,10 +18,23 @@ class Queue:
             self.rear = node
         else:
             self.rear.link = node
-            self.rear = node
+            self.rear = node    # move
 
+
+    def dequeue(self):    # dequeue - 빼기
+        if self.front is None:  # q의 front 값이 none
+            raise IndexError("Queue is empty!") # q 비어있다
+        self.size = self.size - 1   # 1개 삭제
+        temp = self.front
+        self.front = self.front.link    # move
+        if self.front is None:
+            self.rear = None
+        return temp.data
 
 q = Queue()
 q.enqueue("Data structure")     # front
 q.enqueue("Database")
 print(q.size, q.front.data, q.rear.data)
+print(q.dequeue())
+print(q.size, q.front.data, q.rear.data)
+print(q.dequeue())
