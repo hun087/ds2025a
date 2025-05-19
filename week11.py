7b91516bf82c2bd569d19b5904e3c3cf34cfa744
cha80537 <cha80537@gmail.com>
2025-05-19 18:03:08 +0900

    v4.6 graph

--- week11.py
+++ week11.py
@@ -1,17 +1,29 @@
 graph = [
-    [0, 1, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0], #0행
     [1, 0, 0, 1, 0, 0, 0, 0],
     [1, 0, 0, 1, 0, 0, 0, 0],
-    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0], #3행 i=3
     [0, 0, 0, 1, 0, 1, 0, 0],
     [0, 0, 0, 1, 1, 0, 0, 0],
     [0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 1, 0]
+    [0, 0, 0, 0, 0, 0, 1, 0]  #7행 i=7
 ]
 
 def dfs(g, i, visited):
+    visited[i] = True
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]: # j -> 0~7
+            dfs(g, j, visited)
+
+def bfs(g, i, visited):
     pass
 
+visited_dfs = [False for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+dfs(graph, 7, visited_dfs)
+print()
+bfs(graph, 4, visited_bfs)
 
-visited_dfs = [0 for _ in range(len(graph))]
-dfs(graph, 0, visited_dfs)
+# 깊이 유선 탐색 dfs
+# 너비 유선 탐색 bfs
